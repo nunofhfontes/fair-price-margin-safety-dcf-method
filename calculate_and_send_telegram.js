e0532cb4397aba8001c715a7a8ad02c213eacdef
nunofhfontes <nunof.h.fontes@gmail.com>
2023-08-17 13:49:16 +0100

    added feature to send to telegram

---
+++ calculate_and_send_telegram.js
@@ -0,0 +1,88 @@
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const fs = require('fs').promises;
+const nodemailer = require('nodemailer');
+const TelegramBot = require('node-telegram-bot-api');
+require('dotenv').config();
+
+const API_KEY = process.env.API_KEY;
+const API_URL = 'https://financialmodelingprep.com/api/v3/discounted-cash-flow/';
+
+// Initialize Telegram bot with your bot token
+const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, { polling: false });
+
+// ... Existing code ...
+
+// Function to send email with results
+async function sendEmail(results) {
+  // ... Your existing email sending code ...
+}
+
+// Function to send Telegram message with results
+async function sendTelegramMessage(results) {
+  try {
+    const chatIds = process.env.TELEGRAM_CHAT_IDS.split(',');
+
+    const message = `All Stocks Table:\n${formatResults(results.allStocks)}
+High Margin of Safety Stocks:\n${formatResults(results.highMargin)}`;
+
+    for (const chatId of chatIds) {
+      await bot.sendMessage(chatId, message, { parse_mode: 'HTML' });
+      console.log('Telegram message sent to chat ID:', chatId);
+    }
+  } catch (error) {
+    console.error('Error sending Telegram message:', error);
+  }
+}
+
+// ... Remaining functions ...
+
+// Process tickers, send email and Telegram message
+async function processTickersAndNotify() {
+  try {
+    const tickersURL =
+      'https://raw.githubusercontent.com/nunofhfontes/fair-price-margin-safety-dcf-method/master/tickers.txt';
+    const tickers = await fetchTickersFromURL(tickersURL);
+
+    const allStocksTable = [];
+    const highMarginTable = [];
+
+    for (const ticker of tickers) {
+      const stockData = await fetchStockData(ticker);
+      const marginOfSafety = await calculateMarginOfSafety(stockData);
+
+      if (marginOfSafety !== null) {
+        allStocksTable.push({
+          Ticker: ticker,
+          DCF: stockData['dcf'],
+          'Stock Price': stockData['Stock Price'],
+          'Margin of Safety': marginOfSafety,
+        });
+
+        if (parseFloat(marginOfSafety) > 10) {
+          highMarginTable.push({
+            Ticker: ticker,
+            DCF: stockData['dcf'],
+            'Stock Price': stockData['Stock Price'],
+            'Margin of Safety': marginOfSafety,
+          });
+        }
+      }
+    }
+
+    console.table(allStocksTable);
+    console.log('\nHigh Margin of Safety Stocks:');
+    console.table(highMarginTable);
+
+    const results = {
+      allStocks: allStocksTable,
+      highMargin: highMarginTable,
+    };
+
+    //sendEmail(results);
+    sendTelegramMessage(results);
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+processTickersAndNotify();
