284acc4e55d1f9c066f0d85dcc4b3d80cebf399f
nunofhfontes <nunof.h.fontes@gmail.com>
2023-10-06 22:44:38 +0100

    unsynch between blur and suggestion click solved

--- fair_price_next_fe/src/app/components/Searchbox.js
+++ fair_price_next_fe/src/app/components/Searchbox.js
@@ -17,7 +17,12 @@ const SearchBox = () => {
 
   const handleInputBlur = () => {
     console.log('on Blur event');
-    setInputFocused(false);
+    // setInputFocused(false);
+    // Use setTimeout to delay clearing the suggestion
+    setTimeout(() => {
+        setInputFocused(false);
+        setSuggestion(null);
+    }, 100);
   };
 
   const handleSuggestionClick = (suggestion) => {
@@ -65,7 +70,7 @@ const SearchBox = () => {
                 value={inputValue}
                 onChange={handleInputChange}
                 onFocus={handleInputFocus}
-                //onBlur={handleInputBlur}
+                onBlur={handleInputBlur}
             />
             <svg className="w-4 h-4 absolute left-2.5 top-3.5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"/>
