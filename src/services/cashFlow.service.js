8345f3d67ea97501a8aafee63ebf92633b3ab750
nunofhfontes <nunof.h.fontes@gmail.com>
2023-10-23 13:33:58 +0100

    CAGR from years without zero FCF fixed - dirty version

--- src/services/cashFlow.service.js
+++ src/services/cashFlow.service.js
@@ -21,12 +21,12 @@ class CashFlowService {
 
         //const nonZeroYears = Object.keys(data).filter((year) => data[year] !== 0);
         
-        const nonZeroYears = Array.from(data.keys()).filter((year) => data[year] !== 0);
+        const nonZeroYears = Array.from(data.keys()).filter((year) => data.get(year) !== 0);
 
         //const nonZeroYears = data;
 
         //console.log("keys: ", data.keys);
-        console.log("nonZeroYears: ", nonZeroYears);
+        console.log("===>>> nonZeroYears: ", nonZeroYears);
 
         if (nonZeroYears.length < 2) {
             console.warn("CAGR computing without zeros - Not enough data to calculate CAGR");
@@ -34,12 +34,21 @@ class CashFlowService {
         }
         
         const firstYear = nonZeroYears[0];
+        console.log(' firstYear', firstYear);
+        console.log('typeof firstYear', typeof firstYear);
         const lastYear = nonZeroYears[nonZeroYears.length - 1];
-        const initialValue = data[firstYear];
-        const finalValue = data[lastYear];
+        console.log('typeof lastYear', typeof lastYear);
+        console.log('data[firstYear] = ', data.get(firstYear));
+        const initialValue = data.get(firstYear);
+        console.log(' initialValue', initialValue);
+        const finalValue = data.get(lastYear);
+        console.log(' finalValue', finalValue);
         
         const years = lastYear - firstYear + 1;
+        console.log(' years', years);
+        console.log('pre cagr: (finalValue / initialValue) ** (1 / years) : ', (finalValue / initialValue) );
         const cagr = ((finalValue / initialValue) ** (1 / years) - 1) * 100;
+        console.log('typeof cagr', typeof cagr);
         return cagr;
     }
     
